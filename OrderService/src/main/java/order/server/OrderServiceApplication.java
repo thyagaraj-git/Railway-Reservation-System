f4101eda491a4ed90b7f7547d739628ea0027b9d
thyagc <thyagc@LIN25007923.corp.capgemini.com>
2021-12-08 10:54:18 +0530

    added orderservice

---
+++ OrderService/src/main/java/order/server/OrderServiceApplication.java
@@ -0,0 +1,26 @@
+package order.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RequestMapping("/orderService")
+public class OrderServiceApplication {
+	
+	@GetMapping("/order")
+	public String show() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ OrderService/src/test/java/order/server/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package order.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
